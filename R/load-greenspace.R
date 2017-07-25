bb06a9d2c6551285a78a40db62fcb6e74f7ff11a
Robin Lovelace <rob00x@gmail.com>
2017-07-25 12:58:54 +0100

    Update greenspace code

--- R/load-greenspace.R
+++ R/load-greenspace.R
@@ -1,9 +1,24 @@
 library(sf)
+library(dplyr)
+
 g_data = "../OSOpenGreenspace_GB.gml" # the data source
 st_layers(g_data)
 g_point = st_read(g_data, layer = "AccessPoint")
 g_poly = st_read(g_data, layer = "GreenspaceSite")
+st_bbox(g_poly)
+g_poly$area = st_area(g_poly)
+summary(g_poly$area)
 summary(g_point)
 summary(g_poly)
 summary(g$geometry)
 plot(g[1:9,])
+
+
+# analysis
+g_poly_large = g_poly %>% 
+  filter(as.numeric(area) > 1e6)
+plot(g_poly_large$geometry)
+mapview::mapview(g_poly_large)
+library(tmap)
+tmap_mode("view")
+qtm(g_poly_large, fill = "function.")
