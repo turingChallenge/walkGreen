63528842b2e7a988ec1409778b87479dce1ed001
Robin Lovelace <rob00x@gmail.com>
2017-07-25 11:57:52 +0100

    More explicit name

--- R/load-greenspace.R
+++ R/load-greenspace.R
@@ -3,6 +3,7 @@ g_data = "../OSOpenGreenspace_GB.gml" # the data source
 st_layers(g_data)
 g_point = st_read(g_data, layer = "AccessPoint")
 g_poly = st_read(g_data, layer = "GreenspaceSite")
-summary(g)
+summary(g_point)
+summary(g_poly)
 summary(g$geometry)
 plot(g[1:9,])
